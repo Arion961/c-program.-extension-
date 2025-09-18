e6ad83f28123dd98b45d51b348cff5a2d27f9a78
Arion961 <mwikaliarion5520@gmail.com>
2025-09-18 06:57:05 +0300

    Add files via upload

---
+++ units.cxx
@@ -0,0 +1,21 @@
+//simple c program (hello c)
+/*
+Marion Mwikali Mutisya 
+CT100/G/26273/25
+UNITS DONE THIS SEMESTER 
+*/
+
+#include <stdio.h>//pre processor directive scanf 
+
+//main function
+int main (){
+  printf("SPH 2107 ANALOGUE ELECTRONICS\n");
+  printf ("SPI 2101 INTRODUCTION TO PROGRAMMING AND ALGORITHMS \n");
+  printf (" SPI 2103 INTRODUCTION TO COMPUTER APPLICATION\n");
+  printf ("SPI2120 ESSENTIALS OF ACCOUNTING \n");
+  printf ("SPM2104 MATHEMATICS FOR SCIENCE \n");
+  printf ("UCU2105 FUNDAMENTALS OF ICT \n");
+  printf ("UCU2106 COMMUNICATION SKILLS \n");
+  printf ("SPI2102 COMPUTER SYSTEMS AND ORGANISATION\n");
+  return 0;
+}
