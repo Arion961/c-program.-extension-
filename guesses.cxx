d3350c9fa2c68455c327e798ad46971334c1a75e
Arion961 <mwikaliarion5520@gmail.com>
2025-10-09 20:45:40 +0300

    Add files via upload

---
+++ guesses.cxx
@@ -0,0 +1,31 @@
+/*
+MARION MWIKALI
+CT100/G/26273/23
+Guess
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int secret, guess, attempts = 0;
+    
+    srand(time(0));   
+    secret = rand() % 20 + 1;   
+    
+    while (1) {
+        printf("Enter your guess (1-20): ");
+        scanf("%d", &guess);
+        attempts++;
+        
+        if (guess > secret) {
+            printf("Too high!\n");
+        } else if (guess < secret) {
+            printf("Too low!\n");
+        } else {
+            printf("Congratulations! You guessed it in %d attempts.\n", attempts);
+            break;
+        }
+    }
+    return 0;
+}
