db548b9c2582b9bb9e16d97753d66dd8c2bfb722
Arion961 <mwikaliarion5520@gmail.com>
2025-10-09 20:45:14 +0300

    Add files via upload

---
+++ numbers.cxx
@@ -0,0 +1,14 @@
+/*
+MARION MUTISYA
+CT100/G/26273/35
+NUMBERS FROM 100-50
+*/
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
