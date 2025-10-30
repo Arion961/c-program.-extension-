9e15c651c8b94432fbd14cf7da0f3304a692adf1
Arion961 <mwikaliarion5520@gmail.com>
2025-10-30 18:00:48 +0300

    Add files via upload

---
+++ chain.cxx
@@ -0,0 +1,35 @@
+/*
+MARION MWIKALI MUTISYA
+CT100/G/26273/25
+3D ARRAY
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int chain[3][5][10];  
+    int branch, floor, room;
+    int totalOccupied = 0;
+
+    srand(time(0));
+
+    for(branch = 0; branch < 3; branch++) {
+        printf("Branch %d:\n", branch + 1);
+        for(floor = 0; floor < 5; floor++) {
+            printf("  Floor %d: ", floor + 1);
+            for(room = 0; room < 10; room++) {
+                chain[branch][floor][room] = rand() % 2;
+                printf("%d ", chain[branch][floor][room]);
+                if(chain[branch][floor][room] == 1)
+                    totalOccupied++;
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+
+    printf("Total Number of Occupied Rooms Across All Branches: %d\n", totalOccupied);
+
+    return 0;
+}
---
+++ revenue.cxx
@@ -0,0 +1,25 @@
+/*
+MARION MWIKALI MITISYA 
+CT100/G/26273/25
+1D ARRAY
+*/
+#include <stdio.h>
+
+int main() {
+    float revenue[7], total = 0, average;
+    int i;
+
+    printf("Enter the revenue for each day of the week:\n");
+    for(i = 0; i < 7; i++) {
+        printf("Day %d: ", i + 1);
+        scanf("%f", &revenue[i]);
+        total += revenue[i];
+    }
+
+    average = total / 7;
+
+    printf("\nTotal Weekly Revenue = %.2f\n", total);
+    printf("Average Daily Revenue = %.2f\n", average);
+
+    return 0;
+}
